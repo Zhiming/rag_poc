b34f48644e79b56ddecc322a9604002c0e83ac63
Zhiming <athrun07091988@gmail.com>
2026-03-15 11:57:25 -0400

    Remove AWS key to use profile

--- chat_model.py
+++ chat_model.py
@@ -8,8 +8,6 @@ load_dotenv()
 
 class Chat_Model():
     def __init__(self):
-        aws_access_key = os.getenv('AWS_ACCESS_KEY_ID')
-        aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
         aws_region = os.getenv('AWS_REGION')
         model_id = os.getenv("MODEL_ID")
 
@@ -17,8 +15,6 @@ class Chat_Model():
             model=model_id,
             provider="anthropic",
             region_name=aws_region,
-            aws_access_key_id=aws_access_key,
-            aws_secret_access_key=aws_secret_key,
             temperature=0
         )
 
