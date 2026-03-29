a3e793c1ab8bd52a8ae1971c9a60438fe531e068
Zhiming <athrun07091988@gmail.com>
2026-03-28 23:10:12 -0400

    Add scripts to run embedding creation and ES persistency lambdas

---
+++ scripts/run_generate_embeddings.py
@@ -0,0 +1,4 @@
+from lambdas.generate_embeddings import handler
+
+result = handler({}, None)
+print(f"\nSummary: {result}")
---
+++ scripts/run_index_evaluation_notes.py
@@ -0,0 +1,4 @@
+from lambdas.index_evaluation_notes import handler
+
+result = handler({}, None)
+print(f"\nSummary: {result}")
